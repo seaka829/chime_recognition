6916c0acdf98a30df15d61664e1f28315c39c49c
System Administrator <root@seakanoMacBook-Pro.local>
2020-05-10 00:28:16 +0900

    取得した音声データをプロットする機能を実装

--- chime_recognition/chime_recognition.py
+++ chime_recognition/chime_recognition.py
@@ -1,16 +1,20 @@
 import pyaudio
 import numpy as np
+import matplotlib.pyplot as plt
 
 FORMAT      = pyaudio.paInt16
-CHANNELS    = 1
-RATE        = 48000
+CHANNELS    = 1         # maxInputChannels
+RATE        = 48000     # defaultSampleRate
 CHUNK       = 1024
-IDINDEX     = 2
+IDINDEX     = 2         # index
+
+count = 0
+x = []
+y = []
 
 audio = pyaudio.PyAudio()
-
 stream = audio.open(
-    format = FORMAT
+     format = FORMAT
     ,channels = CHANNELS
     ,rate = RATE
     ,frames_per_buffer=CHUNK
@@ -18,19 +22,22 @@ stream = audio.open(
     ,output=False
 )
 
-print('start')
-
 while stream.is_active():
     try:
         input = stream.read(CHUNK, exception_on_overflow=False)
         ndarray = np.frombuffer(input, dtype='int16')
-        for i in range(len(ndarray)):
-            data = np.asscalar(ndarray[i])
-            print(i, data)
+        for i in ndarray:
+            count = count + 1
+            data = i.item()
+            seconds = count/RATE    # 経過時間を取得
+            x.append(seconds)
+            y.append(data)
+            # print(seconds, data)
     except KeyboardInterrupt:
         break
 
-print('end')
+plt.plot(x, y)
+plt.show()
 
 stream.stop_stream()
 stream.close()
